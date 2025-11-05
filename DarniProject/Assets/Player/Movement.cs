1e92269fbdbf26f512615d5f984d331192a2bab5
PerniShoes <marek2001lenss@gmail.com>
2025-11-05 10:57:11 +0100

    Added movement script to player

--- DarniProject/Assets/Player/Movement.cs
+++ DarniProject/Assets/Player/Movement.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+
+
+    public float speed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,19 +17,19 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W))
         {
-        
+        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z + speed * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-
+        transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-
+        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
         }
 
 
