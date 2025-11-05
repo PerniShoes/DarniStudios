c20a9abe7497a7ad7d69891822a5c05776ba7977
PerniShoes <marek2001lenss@gmail.com>
2025-11-05 10:45:20 +0100

    Hero movement setup, Changed camera pos, added colliders

---
+++ DarniProject/Assets/Player/Movement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+        
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+
+
+    }
+}
