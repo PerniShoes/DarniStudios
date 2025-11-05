6ee0823ef707b4449e78ab86805142b67b408320
PerniShoes <marek2001lenss@gmail.com>
2025-11-05 12:00:53 +0100

    movement animation basics

--- DarniProject/Assets/Player/Movement.cs
+++ DarniProject/Assets/Player/Movement.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-
+    public Animator animator;
     public float speed = 30f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // speed = 31f;
+        // speed = 31f;
+        animator.SetBool("isSpawned", true);
     }
 
     // Update is called once per frame
@@ -17,19 +18,23 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-        transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z + speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z + speed * Time.deltaTime);
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.A))
         {
-        transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.S))
         {
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+            animator.SetBool("isMoving", true);
         }
         if (Input.GetKey(KeyCode.D))
         {
         transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            animator.SetBool("isMoving", true);
         }
 
 
