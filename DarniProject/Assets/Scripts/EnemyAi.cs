ab9ba826c9a426855d32ebee15ebc0321e8b449b
PerniShoes <marek2001lenss@gmail.com>
2025-11-06 12:28:58 +0100

    Player movement dash added and UI

--- DarniProject/Assets/Scripts/EnemyAi.cs
+++ DarniProject/Assets/Scripts/EnemyAi.cs
@@ -2,29 +2,43 @@
 
 public class EnemyAI : MonoBehaviour
 {
-
+    public Transform player;      
     public float moveSpeed = 3.5f;
-    public float rotateSpeed = 10f;
+    public float rotateSpeed = 5f;
+    public float attackRange = 2f;
 
-    public Transform player;
+    public Animator animator;    
 
     void Update()
     {
+        // if there is no player, do nothing
         if (player == null) return;
 
+        // calculate direction to the player (only on the XZ plane)
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0f;
 
-        Vector3 toPlayer = player.position - transform.position;
-        toPlayer.y = 0f;
-        if (toPlayer.sqrMagnitude < 0.0001f) return;
+        // rotate smoothly towards the player
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed * Time.deltaTime);
 
-        Quaternion targetRot = Quaternion.LookRotation(toPlayer.normalized, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, rotateSpeed * Time.deltaTime);
+        // calculate distance to the player
+        float distance = direction.magnitude;
 
+        // if player is far → run
+        if (distance > attackRange)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            
+            animator.SetBool("isMoveing", true);
+            animator.SetBool("isAttack", false);
+        }
+        else
+        {
+            // if close → attack
+            animator.SetBool("isMoveing", false);
+            animator.SetBool("isAttack", true);
+        }
     }
-    // DODAC ANIMACJE DO ENEMY ABY PODARZAL ZA GRACZEM A NIE 
-
-
-
 }
