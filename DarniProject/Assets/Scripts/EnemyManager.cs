a2be00d4728fbe8f483162fd2f731b0b5eb068f7
PerniShoes <marek2001lenss@gmail.com>
2025-12-07 10:50:42 +0100

    Testing

--- DarniProject/Assets/Scripts/EnemyManager.cs
+++ DarniProject/Assets/Scripts/EnemyManager.cs
@@ -65,7 +65,7 @@ public class EnemyManager : MonoBehaviour
     public static int aliveEnemies = 0;
     public GameObject[] enemyPrefabs;
 
-    static public int maxAlive = 25;
+    static public int maxAlive = 50;
     EnemyData[] enemies = new EnemyData[maxAlive];
     EnemyViewData[] enemyViewData = new EnemyViewData[maxAlive];
     private int availableSlot = 0;
